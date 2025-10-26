94d15c052a6dbc584644bb38523a5244a9684fdd
周柏諭 <ujhy669917@gmail.com>
2025-10-26 16:57:03 +0800

    cors test v6

--- server.js
+++ server.js
@@ -11,15 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
-
-app.use(cors({ origin: 'http://localhost:5173' }));
-app.options('*', cors());
+app.use(cors({ origin: '*' }));
 
 app.use(express.json()); //自動解析 Content-Type: application/json 的請求 body
 
